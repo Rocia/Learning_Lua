57d8949c7ca4774036f2a0b780ee3db10d214c24
Rocia <rocia.fernandes@gmail.com>
2018-02-15 20:25:31 +0530

    factorial function in lua

---
+++ factorial.lua
@@ -0,0 +1,19 @@
+    function fact (n)
+      if n == 0 then
+        return 1
+      else
+        return n * fact(n-1)
+      end
+    end
+    
+    print("enter a number:")
+    a = io.read("*number")        -- read a number
+    print(fact(a))
+
+--[[
+
+enter a number:
+5
+120
+
+]]
